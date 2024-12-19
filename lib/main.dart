004aae347214be095304a089530093f354ccce7e
leminecraftien2.0 <leminecraftien2.0@gmail.com>
2024-12-19 08:30:07 +0000

    clean code

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/Theme/theme_provider.dart';
 import 'package:myapp/Pages/profile_setup_page.dart';
-import '/creer_compte.dart';
-import '/LoginPage.dart';
 import 'package:myapp/Pages/welcome_page.dart';
 import '../Pages/register_page.dart';
 import '../Pages/signin_page.dart';
@@ -29,59 +26,6 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const SigninPage(),
         '/profil': (context) => const ProfileSetupPage()
       },
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Accueil'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/login'); // Navigation vers le formulaire
-                Navigator.pushNamed(
-                    context, '/login'); // Navigation vers le formulaire
-              },
-              child: const Text('Authentification'),
-            ),
-            const SizedBox(height: 16), // Add some space between the buttons
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/register'); // Navigation vers le formulaire
-              },
-              child: const Text('Créer un compte'),
-            ),
-            const SizedBox(height: 16), // Add some space between the buttons
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/register'); // Navigation vers le formulaire
-              },
-              child: const Text('Créer un compte'),
-            ),
-            const SizedBox(height: 16), // Add some space between the buttons
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/profil'); // Navigation vers le formulaire
-              },
-              child: const Text('profile'),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
