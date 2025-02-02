d04955bd2a2fe69227cf00c2cb15a7e3ea5be549
Léo Wadin <46723431+ArKc0s@users.noreply.github.com>
2025-02-02 19:06:46 +0100

    fix: corrections des routes et du token

--- lib/Services/challenge_service.dart
+++ lib/Services/challenge_service.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:myapp/user_state.dart';
 import '../Models/challenge_dto.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 
 class ChallengeService {
