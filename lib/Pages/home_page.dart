12b95083bfb1aecc018790341579c733c41a1c42
robjo82 <rjrdutg@gmail.com>
2025-02-03 09:23:56 +0100

    feat: display strava activity on home page
    This commit adds the display of the last Strava activity on the home page.
    - Fetches the last activity using the Strava API.
    - Displays activity details: name, date, distance.
    - Shows a static map image of the activity's route.
    - Handles cases where there is no data or an error occurs when fetching data.
    - The activity card is clickable and redirects to the news page.
    - Added a function to build the static map URL.

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,13 +1,19 @@
 import 'dart:convert';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:myapp/Components/main_frame.dart';
 import 'package:myapp/Models/event_dto.dart';
 import 'package:myapp/Services/event_service.dart';
 import 'package:myapp/Theme/app_colors.dart';
 import 'package:myapp/Theme/app_text_styles.dart';
-import 'package:provider/provider.dart';
 import 'package:myapp/user_state.dart';
+import 'package:provider/provider.dart';
+import 'package:strava_client/strava_client.dart';
+
+import '../Services/strava_service.dart';
+import '../secret.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,11 +24,24 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late Future<List<EventDTO>> _futureEvents;
+  late Future<SummaryActivity?> _futureLastActivity;
 
   @override
   void initState() {
     super.initState();
     _futureEvents = EventService().fetchRecentEvents(context);
+    _futureLastActivity = _fetchLastActivity();
+  }
+
+  Future<SummaryActivity?> _fetchLastActivity() async {
+    final isConnected = await StravaService().hasSavedToken();
+    if (!isConnected) return null;
+    final activities = await StravaService()
+        .getActivities(page: 1, perPage: 1, daysBefore: 60);
+    if (activities.isNotEmpty) {
+      return activities.first;
+    }
+    return null;
   }
 
   @override
@@ -45,17 +64,40 @@ class _HomePageState extends State<HomePage> {
                 style: AppTextStyles.headline1,
               ),
               const SizedBox(height: 16.0),
-              SizedBox(
-                height: 150,
-                child: Center(
-                    child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12), // Rayon des coins
-                  child: Image.network(
-                    'https://static.vecteezy.com/system/resources/thumbnails/022/966/737/small_2x/urban-city-map-town-streets-gps-navigation-downtown-map-with-roads-parks-and-river-abstract-roadmap-navigations-scheme-illustration-vector.jpg',
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                )),
+              FutureBuilder<SummaryActivity?>(
+                future: _futureLastActivity,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const SizedBox(
+                      height: 150,
+                      child: Center(child: CircularProgressIndicator()),
+                    );
+                  } else if (snapshot.hasError ||
+                      !snapshot.hasData ||
+                      snapshot.data == null) {
+                    return SizedBox(
+                      height: 150,
+                      child: Center(
+                          child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        // Rayon des coins
+                        child: Image.network(
+                          'https://static.vecteezy.com/system/resources/thumbnails/022/966/737/small_2x/urban-city-map-town-streets-gps-navigation-downtown-map-with-roads-parks-and-river-abstract-roadmap-navigations-scheme-illustration-vector.jpg',
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                    );
+                  } else {
+                    final activity = snapshot.data!;
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/news');
+                      },
+                      child: _buildActivityCard(activity),
+                    );
+                  }
+                },
               ),
               const SizedBox(height: 16.0),
               const Text(
@@ -158,13 +200,20 @@ class _HomePageState extends State<HomePage> {
                               lineBarsData: [
                                 LineChartBarData(
                                   spots: [
-                                    const FlSpot(0, 3.2), // Lundi : 3.2 km
-                                    const FlSpot(1, 5.1), // Mardi : 5.1 km
-                                    const FlSpot(2, 2.8), // Mercredi : 2.8 km
-                                    const FlSpot(3, 6.0), // Jeudi : 6.0 km
-                                    const FlSpot(4, 4.2), // Vendredi : 4.2 km
-                                    const FlSpot(5, 7.3), // Samedi : 7.3 km
-                                    const FlSpot(6, 5.8), // Dimanche : 5.8 km
+                                    const FlSpot(0, 3.2),
+                                    // Lundi : 3.2 km
+                                    const FlSpot(1, 5.1),
+                                    // Mardi : 5.1 km
+                                    const FlSpot(2, 2.8),
+                                    // Mercredi : 2.8 km
+                                    const FlSpot(3, 6.0),
+                                    // Jeudi : 6.0 km
+                                    const FlSpot(4, 4.2),
+                                    // Vendredi : 4.2 km
+                                    const FlSpot(5, 7.3),
+                                    // Samedi : 7.3 km
+                                    const FlSpot(6, 5.8),
+                                    // Dimanche : 5.8 km
                                   ],
                                   isCurved: true,
                                   color: Colors.blueAccent,
@@ -246,6 +295,73 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+}
+
+Widget _buildActivityCard(SummaryActivity act) {
+  final DateFormat dateFormat = DateFormat('dd/MM/yyyy HH:mm');
+  final dateTime = DateTime.parse(act.startDateLocal!).toLocal();
+  final dateStr = dateFormat.format(dateTime);
+  final distanceKm = ((act.distance ?? 0) / 1000.0).toStringAsFixed(2);
+  final polyline = act.map?.summaryPolyline;
+
+  return Container(
+    height: 300,
+    margin: const EdgeInsets.symmetric(vertical: 8),
+    padding: const EdgeInsets.all(16),
+    decoration: BoxDecoration(
+      color: AppColors.lightDark,
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          act.name ?? 'Activité sans nom',
+          style: AppTextStyles.headline2,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          'Date : $dateStr',
+          style: AppTextStyles.hintText,
+        ),
+        Text(
+          'Distance : $distanceKm km',
+          style: AppTextStyles.hintText,
+        ),
+        if (polyline != null && polyline.isNotEmpty)
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(top: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: AppColors.dark,
+              ),
+              child: Image.network(
+                buildStaticMapUrl(polyline),
+                fit: BoxFit.cover,
+                errorBuilder: (ctx, error, stack) => const Center(
+                  child: Text(
+                    'Impossible de charger la carte',
+                    style: TextStyle(color: AppColors.textPrimary),
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
+    ),
+  );
+}
+
+String buildStaticMapUrl(String encodedPolyline) {
+  const apiKey = googleMapsKey;
+  final params = {
+    'size': '600x300',
+    'path': 'enc:$encodedPolyline',
+    'key': apiKey,
+  };
+  final uri = Uri.https('maps.googleapis.com', '/maps/api/staticmap', params);
+  return uri.toString();
 }
 
 Widget _buildStatItem({
